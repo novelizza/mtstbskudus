9578f3a8d5b59fa0c0b9afe9253c78c511d7993d
Rico Eka Rizkya <ricorizkya26@gmail.com>
2023-03-09 20:19:35 +0700

    update api

--- ppdb/login.php
+++ ppdb/login.php
@@ -40,10 +40,31 @@
             
         } else {
             // handle error response
-            echo $response;
+            // echo $response;
             echo 'Error: ' . $object->response . '<br>';
             echo 'Message: ' . $object->message . '<br>';
         }
+
+        echo $response;
+
+        if($response){
+            $_SESSION['logged_in'] = true;
+            $_SESSION['login_time'] = time();
+            $_SESSION['username'] = $username;
+            $_SESSION['session'] = $session;
+            $_SESSION['session_expiry'] = $session_expiry;
+            echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                <strong>Login Berhasil! Pastikan Untuk Mengingat Username dan Password Anda</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+            sleep(3);
+            header("location: beranda.php");
+        }else {
+            echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+            <strong>Username atau Password Salah!</strong>
+            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+          </div>";
+        }
+        curl_close($curl);
     }
 
         
@@ -55,24 +76,7 @@
             // echo $response;
             // echo $session_expiry;
 
-        // if($response){
-        //     $_SESSION['logged_in'] = true;
-        //     $_SESSION['login_time'] = time();
-        //     $_SESSION['username'] = $username;
-        //     $_SESSION['session'] = $session;
-        //     $_SESSION['session_expiry'] = $session_expiry;
-        //     echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
-        //         <strong>Login Berhasil! Pastikan Untuk Mengingat Username dan Password Anda</strong>
-        //         <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
-        //     sleep(3);
-        //     header("location: beranda.php");
-        // }else {
-        //     echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
-        //     <strong>Username atau Password Salah!</strong>
-        //     <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-        //   </div>";
-        // }
-        // curl_close($curl);
+        
 
     // if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
     //     echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
