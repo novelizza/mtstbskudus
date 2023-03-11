687755aab311793a77df59a6f124a1e3ed264d9d
Rico Eka Rizkya <ricorizkya26@gmail.com>
2023-03-11 15:23:22 +0700

    update api

--- ppdb/data-diri.php
+++ ppdb/data-diri.php
@@ -2475,7 +2475,7 @@
                                     $objectPPPP = json_decode($responsePPP, true);
                                     
                                     curl_close($curlPRESTASI);
-                                    echo $objectPPPP['response']['result'][0];
+                                    echo $objectPPPP['response']['result'];
                                 ?>
                                 <div class="col-md-12">
                                     <label for=""><b>Tahun</b></label>
