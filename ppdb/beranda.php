1f1b204d3ac4354ed6d4147d420c44af8baec315
Rico Eka Rizkya <ricorizkya26@gmail.com>
2023-03-14 07:09:59 +0700

    update api

--- ppdb/beranda.php
+++ ppdb/beranda.php
@@ -217,13 +217,13 @@
                 </div><br>
                 <div class="row g-3">
                     <?php
-                        if($isLengkap == 0 && $tujuan_masuk == "MPTS") {
+                        if($isLengkap == 0 && $tujuan_masuk === "MPTS") {
                             echo "<div class='col-md-12'>
                                 <a href='cetak-bukti-pembayaran.php' class='btn' style='background-color: grey; color: white; width: 100%;' onclick='return false;'>
                                     <i class='bi bi-printer-fill'></i><span> Cetak Bukti Pembayaran</span>
                                 </a>
                             </div>";
-                        }elseif($isLengkap == 0 && $tujuan_masuk == "MTS") {
+                        }elseif($isLengkap == 0 && $tujuan_masuk === "MTS") {
                             echo "
                                 <div class='col-md-12'>
                                     <a class='btn' href='cetak-kartu-tes.php'
@@ -232,13 +232,13 @@
                                     </a>
                                 </div>
                             ";
-                        }elseif($isLengkap == 1 && $tujuan_masuk == "MPTS") {
+                        }elseif($isLengkap == 1 && $tujuan_masuk === "MPTS") {
                             echo "<div class='col-md-12'>
                                 <a href='cetak-bukti-pembayaran.php' class='btn' style='background-color: grey; color: white; width: 100%;' onclick='return false;'>
                                     <i class='bi bi-printer-fill'></i><span> Cetak Bukti Pembayaran</span>
                                 </a>
                             </div>";
-                        }elseif($isLengkap == 1 && $tujuan_masuk == "MTS") {
+                        }elseif($isLengkap == 1 && $tujuan_masuk === "MTS") {
                             echo "
                                 <div class='col-md-12'>
                                     <a class='btn' href='cetak-kartu-tes.php'
