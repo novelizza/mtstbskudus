273e05b3ee9a62e1197646e338a3ba6866a78854
Rico Eka Rizkya <ricorizkya26@gmail.com>
2023-03-15 03:21:55 +0700

    update api

--- admin/tambah-admin.php
+++ admin/tambah-admin.php
@@ -7,6 +7,8 @@
     $session_expiry = $_SESSION['session_expiry'];
 
     if(isset($_POST['tambah_admin'])) {
+        echo "<script>alert('INPUT DATA PRESTASI 3 GAGAL! ULANGI LAGI DAN PASTIKAN DATA YANG ANDA MASUKKAN SUDAH BENAR!');</script>";
+
         $nama_lengkap = $_POST['nama_admin'];
         $nip = $_POST['nip_admin'];
         $username_admin = $_POST['username_admin'];
